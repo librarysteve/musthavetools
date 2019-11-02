579b5c9488cc599eca2393db1d2441540cfdacf6
LibrarySteve <stephenkylegarrison@gmail.com>
2019-11-02 03:32:49 -0400

    Update installmymusthavetools.sh

--- installmymusthavetools.sh
+++ installmymusthavetools.sh
@@ -3,3 +3,6 @@ USER=$(whoami)
 read - p "Install ALLLLLLLL the tools? [y/n]: " GO
 
 if [[ $GO == 'y' ]]; then
+  if [[ USER == 'root' ]]; then
+    apt-get update && apt-get upgrade -y
+    apt-get install python python3 python-pip python3-pip git
